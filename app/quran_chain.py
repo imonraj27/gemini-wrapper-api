529554b5487f1af71b22fb0ad2072f4ef9f2cc70
imonraj27 <77281501+imonraj27@users.noreply.github.com>
2025-04-18 16:34:45 +0530

    dsfsd

--- app/quran_chain.py
+++ app/quran_chain.py
@@ -9,7 +9,7 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-001", temperature=0.5)
 
 prompt = PromptTemplate(
     input_variables=["question"],
-    template="Please provide an answer from the Quran for the following question: {question}. Answer should be based only on Quranic teachings or islamic authentic sources(Hadid). Answer in Bengali even when you don't know."
+    template="Please provide an answer from the Quran or authentic islamic sources(Hadid) or from both for the following question: {question}. Answer should be based only on Quranic teachings or islamic authentic sources(Hadid). Answer in Bengali even when you don't know."
 )
 
 chain = prompt | llm
